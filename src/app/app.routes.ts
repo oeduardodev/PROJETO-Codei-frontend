b6071f0372ca6f4b6d7f9c4407cb62e59a0ea138
Eduardo Leite <eduardo.leite@datacoress.com>
2024-03-15 11:29:52 -0300

    FEAT: Estruturando componentes de layout .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
-import { Routes } from '@angular/router';
+
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { NgModule } from '@angular/core';
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'moments/new', component: NewMomentComponent }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
