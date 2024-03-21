920c420db6250656c0dd6660541bd09bbb275e94
Eduardo Leite <eduardo.leite@datacoress.com>
2024-03-21 13:41:17 -0300

    FEAT: Tela de detalhes do momento.

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
 import { MomentService } from '../../../services/moment.service';
 import { Moment } from '../../../Moments';
-
+import { environment } from '../../../environment/environments';
 import { CommonModule } from '@angular/common';
+import { faTimes, faEdit, } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-moment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './moment.component.html',
   styleUrl: './moment.component.css'
 })
 export class MomentComponent {
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl
+
+  faEdit = faEdit
+  faTimes = faTimes
 
   constructor(
     private momentService: MomentService,
