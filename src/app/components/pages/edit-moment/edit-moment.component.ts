74483be3f757ab92ed29d63a38c42b3ef8582e52
Eduardo Leite <eduardo.leite@datacoress.com>
2024-03-22 12:08:20 -0300

    FEAT: Finalizando componente de edição de momento.

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 import { Moment } from '../../../Moments';
 
 import { MomentService } from '../../../services/moment.service';
+import { MessageService } from '../../../services/message.service';
 
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MomentFormComponent } from '../../moment-form/moment-form.component';
 
@@ -22,15 +22,35 @@ export class EditMomentComponent {
 
   constructor(
     private momentService: MomentService,
-    private route: ActivatedRoute
-    ) {}
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get("id"));
 
     this.momentService.getMoment(id).subscribe((item) => {
-      this.moment = item.data;  
+      this.moment = item.data;
     })
+  }
+
+  async editHandler(momentData: Moment) {
+    const id = this.moment.id
+    const formData = new FormData()
+
+    formData.append('title', momentData.title)
+    formData.append('description', momentData.description)
+
+    if (momentData.image) {
+      formData.append('image', momentData.image)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe()
+
+    this.messageService.addMessage(`momento ${id} foi atualizado.`)
+
+    this.router.navigate(['/'])
 
   }
 }
