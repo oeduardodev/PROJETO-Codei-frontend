f0c8be3e517266debc0f8024d138065b694c9cc9
oeduardodev <duduleite0@gmail.com>
2026-01-22 23:14:47 -0300

    FIX: Mensagens de alerta bno form erradas

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,73 +1,85 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { Moment } from '../../models/Moments';
-import { UsersService } from '../../services/users.service';
-import Cropper from 'cropperjs';
-import { AuthorizationService } from '../../services/auth.service';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { Moment } from "../../models/Moments";
+import { UsersService } from "../../services/users.service";
+import Cropper from "cropperjs";
+import { AuthorizationService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-moment-form',
+  selector: "app-moment-form",
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
-  templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  templateUrl: "./moment-form.component.html",
+  styleUrls: ["./moment-form.component.css"],
 })
 export class MomentFormComponent implements OnInit {
   @Output() OnSubmit = new EventEmitter<FormData>();
   @Input() btnText!: string;
   @Input() momentData!: Moment;
-  @ViewChild('imageCropper', { static: false }) imageElement!: ElementRef;
+  @ViewChild("imageCropper", { static: false }) imageElement!: ElementRef;
 
-  imageUrl = '';
+  imageUrl = "";
   cropper!: Cropper;
   croppedImage: string | null = null;
 
   momentForm!: FormGroup;
   userId?: number;
-
-  token: string = localStorage.getItem('authToken') || '';
+  isImageLoading = false;
 
   constructor(
     private usersService: UsersService,
-    private cdr: ChangeDetectorRef,
-    private authService: AuthorizationService
-  ) { }
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.getUserInfo();
     this.momentForm = new FormGroup({
-      id: new FormControl(this.momentData ? this.momentData.id : ''),
-      title: new FormControl(this.momentData ? this.momentData.title : '', [Validators.required]),
-      description: new FormControl(this.momentData ? this.momentData.description : '', [Validators.required]),
-      image: new FormControl(''),
-      user_id: new FormControl('')
+      id: new FormControl(this.momentData ? this.momentData.id : ""),
+      title: new FormControl(this.momentData ? this.momentData.title : "", [
+        Validators.required,
+      ]),
+      description: new FormControl(
+        this.momentData ? this.momentData.description : "",
+        [Validators.required]
+      ),
+      image: new FormControl(""),
+      user_id: new FormControl(""),
     });
   }
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files[0]) {
+      this.isImageLoading = true;
+
       const reader = new FileReader();
       reader.onload = (e) => {
-        this.imageUrl = (e.target?.result as string) || '';
+        this.imageUrl = (e.target?.result as string) || "";
 
-        this.cdr.detectChanges();        
+        this.cdr.detectChanges();
         this.initializeCropper();
-
       };
       reader.readAsDataURL(input.files[0]);
     }
   }
 
   initializeCropper(): void {
-  
     if (!this.imageElement?.nativeElement) {
-      console.error('Image element not found.');
+      console.error("Image element not found.");
       return;
     }
-  
+
     this.cropper = new Cropper(this.imageElement.nativeElement, {
       aspectRatio: 1,
       viewMode: 1,
@@ -80,24 +92,23 @@ export class MomentFormComponent implements OnInit {
   }
 
   getUserInfo() {
-
     this.usersService.getUser().subscribe(
       (data) => {
         this.userId = data.id;
         this.momentForm.patchValue({ user_id: this.userId });
       },
       (error) => {
-        console.error('Error fetching user info', error);
+        console.error("Error fetching user info", error);
       }
     );
   }
-  
+
   get title() {
-    return this.momentForm.get('title')!;
+    return this.momentForm.get("title")!;
   }
 
   get description() {
-    return this.momentForm.get('description')!;
+    return this.momentForm.get("description")!;
   }
   async cropImage(): Promise<File | null> {
     return new Promise((resolve) => {
@@ -106,51 +117,53 @@ export class MomentFormComponent implements OnInit {
           width: 1080,
           height: 1080,
         });
-  
+
         canvas.toBlob((blob) => {
           if (blob) {
-            const file = new File([blob], 'cropped-image.jpg', { type: 'image/jpeg' });
+            const file = new File([blob], "cropped-image.jpg", {
+              type: "image/jpeg",
+            });
             this.momentForm.patchValue({ image: file });
             resolve(file);
           } else {
-            console.error('Erro ao converter canvas para blob.');
+            console.error("Erro ao converter canvas para blob.");
             resolve(null);
           }
-        }, 'image/jpeg');
+        }, "image/jpeg");
       } else {
-        console.error('Cropper não foi inicializado.');
+        console.error("Cropper não foi inicializado.");
         resolve(null);
       }
     });
   }
-  
+
   async submit() {
     const croppedImageFile = await this.cropImage();
     if (!croppedImageFile) {
-      console.error('Erro ao preparar a imagem cortada.');
+      console.error("Erro ao preparar a imagem cortada.");
       return;
     }
 
     if (this.momentForm.invalid) {
-      console.error('O formulário está inválido.');
+      console.error("O formulário está inválido.");
       return;
     }
-  
+
     if (this.userId === undefined) {
-      console.error('User ID não foi definido.');
+      console.error("User ID não foi definido.");
       return;
     }
-  
+
     const formData = new FormData();
-    formData.append('id', this.momentForm.get('id')!.value || '');
-    formData.append('title', this.momentForm.get('title')!.value);
-    formData.append('description', this.momentForm.get('description')!.value);
-    formData.append('user_id', this.userId.toString());
+    formData.append("id", this.momentForm.get("id")!.value || "");
+    formData.append("title", this.momentForm.get("title")!.value);
+    formData.append("description", this.momentForm.get("description")!.value);
+    formData.append("user_id", this.userId.toString());
 
     if (croppedImageFile) {
-      formData.append('image', croppedImageFile);
+      formData.append("image", croppedImageFile);
     } else {
-      console.error('Imagem cortada não está presente no FormData.');
+      console.error("Imagem cortada não está presente no FormData.");
     }
 
     this.OnSubmit.emit(formData);
--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,40 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
-import { MomentService } from '../../../services/moment.service';
-import { MessageService } from '../../../services/message.service';
-import { Moment } from '../../../models/Moments';
-import { Comment } from '../../../models/Comments';
-import { environment } from '../../../environment/environments';
-import { CommonModule } from '@angular/common';
-import { faTimes, faEdit, faUsersRays } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CommentService } from '../../../services/comment.service';
-import { UsersService } from '../../../services/users.service';
-import { AsideProfileComponent } from '../../aside-profile/aside-profile.component';
-import { LikeService } from '../../../services/like.service';
-import { User } from '../../../models/User';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import { MomentService } from "../../../services/moment.service";
+import { MessageService } from "../../../services/message.service";
+import { Moment } from "../../../models/Moments";
+import { Comment } from "../../../models/Comments";
+import { environment } from "../../../environment/environments";
+import { CommonModule } from "@angular/common";
+import {
+  faTimes,
+  faEdit,
+  faUsersRays,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CommentService } from "../../../services/comment.service";
+import { UsersService } from "../../../services/users.service";
+import { AsideProfileComponent } from "../../aside-profile/aside-profile.component";
+import { LikeService } from "../../../services/like.service";
+import { User } from "../../../models/User";
 
 @Component({
-  selector: 'app-moment',
+  selector: "app-moment",
   standalone: true,
-  imports: [AsideProfileComponent, CommonModule, FontAwesomeModule, RouterLink, ReactiveFormsModule],
-  templateUrl: './moment.component.html',
-  styleUrl: './moment.component.css'
+  imports: [
+    AsideProfileComponent,
+    CommonModule,
+    FontAwesomeModule,
+    RouterLink,
+    ReactiveFormsModule,
+  ],
+  templateUrl: "./moment.component.html",
+  styleUrl: "./moment.component.css",
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
   baseApiUrl = environment.endpoint;
-  
-  userNameLog = '';
+
+  userNameLog = "";
   likeAtive = false;
 
   faEdit = faEdit;
   faTimes = faTimes;
   faRay = faUsersRays;
   commentForm!: FormGroup;
-
-  token: string = localStorage.getItem('authToken') || '';
 
   constructor(
     private momentService: MomentService,
@@ -43,8 +57,8 @@ export class MomentComponent implements OnInit {
     private router: Router,
     private commentService: CommentService,
     private userService: UsersService,
-    private likeService: LikeService,
-  ) { }
+    private likeService: LikeService
+  ) {}
 
   ngOnInit(): void {
     this.getMoment();
@@ -55,48 +69,45 @@ export class MomentComponent implements OnInit {
   setupCommentForm() {
     this.commentForm = new FormGroup({
       text: new FormControl("", [Validators.required]),
-      username: new FormControl("", [Validators.required])
+      username: new FormControl("", [Validators.required]),
     });
   }
 
   getUser() {
-  
-    this.userService.getUser().subscribe((user:User) => {
-      this.userNameLog = user.username ?? '';
+    this.userService.getUser().subscribe((user: User) => {
+      this.userNameLog = user.username ?? "";
       if (this.userNameLog) {
         this.commentForm.patchValue({
-          username: this.userNameLog
+          username: this.userNameLog,
         });
-        this.commentForm.get('username')?.disable();  
+        this.commentForm.get("username")?.disable();
       }
     });
   }
 
   getMoment() {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = Number(this.route.snapshot.paramMap.get("id"));
 
     this.momentService.getMoment(id).subscribe((item) => {
-        this.moment = new Moment(item.data);      
-   
+      this.moment = new Moment(item.data);
+
       if (this.moment && this.moment.id !== undefined) {
-        this.likeService.getLike(this.moment.id, this.token).subscribe(
-          (like) => {
-            this.likeAtive = like.liked;
-          }
-        );
+        this.likeService.getLike(this.moment.id).subscribe((like) => {
+          this.likeAtive = like.liked;
+        });
       } else {
-        console.error('Moment or Moment ID is undefined.');
+        console.error("Moment or Moment ID is undefined.");
       }
     });
   }
 
   get text() {
-    return this.commentForm.get('text')!;
+    return this.commentForm.get("text")!;
   }
 
   get username() {
     if (this.userNameLog) {
-      return
+      return;
     }
     this.commentForm.get("username")!;
   }
@@ -105,45 +116,45 @@ export class MomentComponent implements OnInit {
     if (this.commentForm.invalid) {
       return;
     }
-  
+
     // Habilitar o campo username temporariamente se estiver desabilitado
-    const usernameControl = this.commentForm.get('username');
+    const usernameControl = this.commentForm.get("username");
     if (usernameControl?.disabled) {
       usernameControl.enable();
     }
-  
+
     // Verifica se o campo `username` está vazio e preenche com `userNameLog` se necessário
     if (!usernameControl || !usernameControl.value.trim()) {
       this.commentForm.patchValue({ username: this.userNameLog });
     }
-  
+
     const data: Comment = {
       ...this.commentForm.value,
-      momentId: Number(this.moment!.id)
+      momentId: Number(this.moment!.id),
     };
-  
+
     // Desabilitar o campo username novamente se necessário
     if (this.userNameLog) {
       usernameControl?.disable();
     }
-  
+
     this.commentService.createComment(data).subscribe((comment) => {
       this.moment!.comments!.push(comment.data);
     });
-  
+
     this.messagesService.addMessage("Comentário Adicionado");
-  
+
     this.commentForm.reset();
     formDirective.resetForm();
-    this.getMoment(); 
+    this.getMoment();
     this.getUser();
   }
-  
+
   async removeHandler(id: number) {
     this.momentService.removeMoment(id).subscribe();
 
     this.messagesService.addMessage("Momento excluido com sucesso!");
-    this.router.navigate(['/']);
+    this.router.navigate(["/"]);
   }
 
   sendLike() {
@@ -152,49 +163,48 @@ export class MomentComponent implements OnInit {
       this.likeAtive = !this.likeAtive;
 
       // Atualize a imagem imediatamente após a inversão
-      const imageElement = document.querySelector('.capilike') as HTMLImageElement;
+      const imageElement = document.querySelector(
+        ".capilike"
+      ) as HTMLImageElement;
       if (imageElement) {
         imageElement.src = this.updateLikeImage();
       }
 
       // Envie a requisição de like para o servidor
-      this.likeService.sendLike(this.moment.id, this.token).subscribe();
+      this.likeService.sendLike(this.moment.id).subscribe();
       // Opcional: Atualize o estado de like após obter a resposta do servidor
-      this.likeService.getLike(this.moment.id, this.token).subscribe(
-        (like) => {
-          this.likeAtive = like.liked;
-          // Atualize a imagem novamente após receber a resposta do servidor
-          if (imageElement) {
-            imageElement.src = this.updateLikeImage();
-          }
+      this.likeService.getLike(this.moment.id).subscribe((like) => {
+        this.likeAtive = like.liked;
+        // Atualize a imagem novamente após receber a resposta do servidor
+        if (imageElement) {
+          imageElement.src = this.updateLikeImage();
         }
-      );
+      });
     } else {
-      console.error('Moment ID is undefined. Cannot send like.');
+      console.error("Moment ID is undefined. Cannot send like.");
     }
 
     // Animação de clique (opcional)
-    const imageElement = document.querySelector('.capilike');
+    const imageElement = document.querySelector(".capilike");
 
     if (imageElement) {
-      imageElement.classList.add('clicked');
+      imageElement.classList.add("clicked");
       setTimeout(() => {
-        imageElement.classList.remove('clicked');
-        this.getMoment()
-      }, 200); 
+        imageElement.classList.remove("clicked");
+        this.getMoment();
+      }, 200);
     }
-    this.getMoment()
-
+    this.getMoment();
   }
 
   updateLikeImage(): string {
     if (this.moment && this.moment.id !== undefined) {
       return this.likeAtive
-        ? '../../../../assets/capilike-ative.png'
-        : '../../../../assets/capilike.png';
+        ? "../../../../assets/capilike-ative.png"
+        : "../../../../assets/capilike.png";
     } else {
-      console.error('Moment ID is undefined. Cannot get like image.');
-      return '../../../../assets/capilike.png';
+      console.error("Moment ID is undefined. Cannot get like image.");
+      return "../../../../assets/capilike.png";
     }
   }
-}  
+}
